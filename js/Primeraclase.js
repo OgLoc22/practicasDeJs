67653f34d0bb56b62f2e6ce4e7785f71476256a2
core.autocrlf <chriztopher1996@gmail.com>
2022-12-17 15:32:49 -0300

    primer commit

---
+++ js/Primeraclase.js
@@ -0,0 +1,13 @@
+console.log("hola mundo");
+document.write("hola mundo");
+
+console.log("Este es Un Segundo mensaje");
+
+const PI = 3.14;
+
+let nombre = prompt("Esacribe un nombre");
+console.log(nombre);
+
+nombre = "Hielo";
+console.log(nombre);
+ 
